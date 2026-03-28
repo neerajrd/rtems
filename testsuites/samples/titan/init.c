07e2f0715623ccdd62eec84b25db50e8f05b7d35
Neeraj Deshpande <30022081+neerajrd@users.noreply.github.com>
2026-03-28 16:49:26 +0530

    Updated.

--- testsuites/samples/titan/init.c
+++ testsuites/samples/titan/init.c
@@ -1,61 +1,193 @@
-/* SPDX-License-Identifier: BSD-2-Clause */
-
-/*
- *  COPYRIGHT (c) 1989-2012.
- *  On-Line Applications Research Corporation (OAR).
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
 #include <rtems.h>
 #include <tmacros.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-const char rtems_test_name[] = "HELLO WORLD";
+#include <rtems/jffs2.h>
+#include <rtems/libio.h>
 
-static rtems_task Init( rtems_task_argument ignored )
+const char rtems_test_name[] = "HELLO JFFS2";
+
+/* -------- Flash Configuration -------- */
+#define BLOCK_SIZE (32UL * 1024UL)
+#define FLASH_SIZE (32UL * BLOCK_SIZE)
+
+/* -------- Flash Control Structure -------- */
+typedef struct {
+  rtems_jffs2_flash_control super;
+  unsigned char area[FLASH_SIZE];
+} flash_control;
+
+static flash_control flash_instance;
+
+/* -------- Helper -------- */
+static flash_control *get_flash_control(rtems_jffs2_flash_control *super)
 {
-  (void) ignored;
-
-  rtems_print_printer_fprintf_putc( &rtems_test_printer );
-  TEST_BEGIN();
-  printf( "Hello World\n" );
-  TEST_END();
-  rtems_test_exit( 0 );
+  return (flash_control *) super;
 }
 
-/* NOTICE: the clock driver is explicitly disabled */
+/* -------- Flash Ops -------- */
+static int flash_read(
+  rtems_jffs2_flash_control *super,
+  uint32_t offset,
+  unsigned char *buffer,
+  size_t size
+)
+{
+  flash_control *self = get_flash_control(super);
+
+  printf("[FLASH READ] offset=%u size=%zu\n", offset, size);
+
+  if ((offset + size) > FLASH_SIZE)
+    return -1;
+
+  memcpy(buffer, &self->area[offset], size);
+  return 0;
+}
+
+static int flash_write(
+  rtems_jffs2_flash_control *super,
+  uint32_t offset,
+  const unsigned char *buffer,
+  size_t size
+)
+{
+  flash_control *self = get_flash_control(super);
+  size_t i;
+
+  printf("[FLASH WRITE] offset=%u size=%zu\n", offset, size);
+
+  if ((offset + size) > FLASH_SIZE)
+    return -1;
+
+  for (i = 0; i < size; ++i) {
+    self->area[offset + i] &= buffer[i];
+  }
+
+  return 0;
+}
+
+static int flash_erase(
+  rtems_jffs2_flash_control *super,
+  uint32_t offset
+)
+{
+  flash_control *self = get_flash_control(super);
+
+  printf("[FLASH ERASE] offset=%u size=%lu\n", offset, BLOCK_SIZE);
+
+  if ((offset + BLOCK_SIZE) > FLASH_SIZE)
+    return -1;
+
+  memset(&self->area[offset], 0xFF, BLOCK_SIZE);
+  return 0;
+}
+
+/* -------- Compressor -------- */
+static rtems_jffs2_compressor_zlib_control compressor_instance = {
+  .super = {
+    .compress = rtems_jffs2_compressor_zlib_compress,
+    .decompress = rtems_jffs2_compressor_zlib_decompress
+  }
+};
+
+/* -------- Mount Data -------- */
+static const rtems_jffs2_mount_data mount_data = {
+  .flash_control = &flash_instance.super,
+  .compressor_control = &compressor_instance.super
+};
+
+/* -------- Init Task -------- */
+static rtems_task Init(rtems_task_argument ignored)
+{
+  int rv;
+  int fd;
+  const char *msg = "Hello from JFFS2!\n";
+  char read_buf[64] = {0};
+
+  (void) ignored;
+
+  rtems_print_printer_fprintf_putc(&rtems_test_printer);
+  TEST_BEGIN();
+
+  /* Initialize flash */
+  memset(flash_instance.area, 0xFF, FLASH_SIZE);
+
+  /* Setup flash control */
+  flash_instance.super.block_size = BLOCK_SIZE;
+  flash_instance.super.flash_size = FLASH_SIZE;
+  flash_instance.super.read = flash_read;
+  flash_instance.super.write = flash_write;
+  flash_instance.super.erase = flash_erase;
+  flash_instance.super.device_identifier = 0xc01dc0fe;
+
+  /* Mount filesystem */
+  rv = mount_and_make_target_path(
+    NULL,
+    "/jffs2",
+    RTEMS_FILESYSTEM_TYPE_JFFS2,
+    RTEMS_FILESYSTEM_READ_WRITE,
+    &mount_data
+  );
+
+  if (rv != 0) {
+    printf("Mount failed: %s\n", strerror(errno));
+    TEST_END();
+    rtems_test_exit(1);
+  }
+
+  printf("Mounted JFFS2 at /jffs2\n");
+
+  /* -------- File Create & Write -------- */
+  fd = open("/jffs2/test.txt", O_CREAT | O_WRONLY, 0777);
+  if (fd < 0) {
+    printf("File open failed: %s\n", strerror(errno));
+    TEST_END();
+    rtems_test_exit(1);
+  }
+
+  write(fd, msg, strlen(msg));
+  close(fd);
+  printf("File written\n");
+
+  /* -------- File Read -------- */
+  fd = open("/jffs2/test.txt", O_RDONLY);
+  if (fd < 0) {
+    printf("File open (read) failed: %s\n", strerror(errno));
+    TEST_END();
+    rtems_test_exit(1);
+  }
+
+  read(fd, read_buf, sizeof(read_buf) - 1);
+  close(fd);
+
+  printf("Read from file: %s\n", read_buf);
+
+  printf("Hello World\n");
+
+  TEST_END();
+  rtems_test_exit(0);
+}
+
+/* -------- RTEMS Config -------- */
 #define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
 #define CONFIGURE_APPLICATION_NEEDS_SIMPLE_CONSOLE_DRIVER
 
-#define CONFIGURE_MAXIMUM_TASKS 1
+#define CONFIGURE_FILESYSTEM_JFFS2
+#define CONFIGURE_MAXIMUM_FILE_DESCRIPTORS 20
 
+#define CONFIGURE_MAXIMUM_TASKS 2
 #define CONFIGURE_RTEMS_INIT_TASKS_TABLE
 
 #define CONFIGURE_INIT_TASK_ATTRIBUTES RTEMS_FLOATING_POINT
-
 #define CONFIGURE_INITIAL_EXTENSIONS RTEMS_TEST_INITIAL_EXTENSION
 
 #define CONFIGURE_INIT
